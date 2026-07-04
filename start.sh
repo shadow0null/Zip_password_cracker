98d5bfd400bc50916fcfe000d428e7e85c08c187
shadow0null <98105494+shadow0null@users.noreply.github.com>
2026-07-04 09:41:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo -e "\e[1;31m"
 figlet Zip Password cracker
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border QADIR
+toilet -f mono12 -F border shadow0null
 
 echo " "
 apt update
@@ -9,7 +9,7 @@ apt upgrade
 pkg install python
 pkg install python3
 pkg install git
-git clone https://github.com/hacker6291/Zip_password_cracker.git
+git clone https://github.com/shadow0null/Zip_password_cracker.git
 pip install zipfile
 pip install os
 pip install colored
